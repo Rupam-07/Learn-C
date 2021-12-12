736c600d65bc0c2ca4e9483059847c1b10714846
Rupam-07 <rupamkarmakarcr7@gmail.com>
2021-12-12 20:32:43 +0530

    maxMin

---
+++ maxMin.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+int maximum(int num[])
+{
+    int max=num[0];
+    for (int i=0;i<10;i++) {
+        if (num[i] > max){
+            max = num[i];
+        }
+    }
+    return max;
+}
+int minimum(int num[])
+{
+    int min=num[0];
+    for(int i=0;i<10;i++){
+        if(num[i]<min){  //num[0] 10 num[1] 9 num[2] 11 num[3] 8 num[4] 10 num[5] 5
+            min=num[i]; //10 9 8 5 
+        }
+    }
+    return min;
+}
+void main()
+{
+    int num[10];
+    printf("Enter Ten Numbers : ");
+    for (int i = 0; i < 10; i++){
+        scanf("%d", &num[i]);
+    }
+    printf("Maximum Value is : %d\n", maximum(num));
+    printf("minimum value is : %d",minimum(num));
+}
